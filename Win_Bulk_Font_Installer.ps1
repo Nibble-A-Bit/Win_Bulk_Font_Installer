6a402521d3a3e86b707c29760d12ace4a3f13e91
Cornelis Terblanche <1997corry@gmail.com>
2023-07-25 01:56:56 +0200

    Recursively install fonts

--- Win_Bulk_Font_Installer.ps1
+++ Win_Bulk_Font_Installer.ps1
@@ -1,63 +1,70 @@
 param (
-    [Parameter(Mandatory = $true)]
-    [System.IO.FileInfo]$src
+    [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [System.IO.FileInfo[]]$src
 )
 function Install-Font {
     param (
-        [System.IO.FileInfo]$fontFile
+        [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [System.IO.FileInfo[]]$srcDir
     )
 
-    try {
-        # Check if the font file exists
-        if (-not $fontFile.Exists) {
-            throw "Font file not found: $($fontFile.FullName)"
+    process {
+        foreach ($file in $srcDir) {
+            try {
+                # Check if the font file exists
+                if (-not $file.Exists) {
+                    Write-Host "Font file not found: $($file.FullName)"
+                    continue
+                }
+
+                # Get font name and style
+                $fontName = $file.Name
+                switch ($file.Extension.ToLower()) {
+                    ".ttf" { $fontName += " (TrueType)" }
+                    ".otf" { $fontName += " (OpenType)" }
+                }
+
+                # Check if the font is already installed
+                $fontDestinationPath = "$($env:windir)\Fonts\$($file.Name)"
+                if (Test-Path $fontDestinationPath) {
+                    Write-Host "Font already installed: $($file.Name)"
+                    continue
+                }
+
+                # Prompt user for confirmation before proceeding
+                $confirmation = Read-Host "Are you sure you want to install the font '$fontName'? (Y/N)"
+                if ($confirmation -notmatch '^[Yy]$') {
+                    Write-Host "Font installation cancelled."
+                    continue
+                }
+
+                # Copy the font file to the Fonts directory
+                Write-Host "Copying font: $($file.Name)"
+                Copy-Item -Path $file.FullName -Destination $fontDestinationPath
+
+                # Install the font using AddFontResource
+                $result = AddFontResource -LiteralPath $fontDestinationPath
+                [System.Runtime.InteropServices.Marshal]::ReleaseComObject($result) | Out-Null
+
+                # Check if the font is already registered
+                $fontRegistryPath = "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts"
+                if (Get-ItemProperty -Name $fontName -Path $fontRegistryPath -ErrorAction SilentlyContinue) {
+                    Write-Host "Font already registered: $($file.Name)"
+                    continue
+                }
+
+                # Register the font in the system's font registry
+                Write-Host "Registering font: $($file.Name)"
+                New-ItemProperty -Name $fontName -Path $fontRegistryPath -PropertyType String -Value $file.Name -Force | Out-Null
+
+                Write-Host "Font installed successfully: $($file.Name)"
+            } catch {
+                Write-Host "Error installing font: $($file.Name). $_"
+            }
         }
-
-        # Get font name and style
-        $fontName = $fontFile.Name
-        switch ($fontFile.Extension.ToLower()) {
-            ".ttf" { $fontName += " (TrueType)" }
-            ".otf" { $fontName += " (OpenType)" }
-        }
-
-        # Check if the font is already installed
-        $fontDestinationPath = "$($env:windir)\Fonts\$($fontFile.Name)"
-        if (Test-Path $fontDestinationPath) {
-            Write-Host "Font already installed: $($fontFile.Name)"
-            return
-        }
-
-        # Prompt user for confirmation before proceeding
-        $confirmation = Read-Host "Are you sure you want to install the font '$fontName'? (Y/N)"
-        if ($confirmation -notmatch '^[Yy]$') {
-            Write-Host "Font installation cancelled."
-            return
-        }
-
-        # Copy the font file to the Fonts directory
-        Write-Host "Copying font: $($fontFile.Name)"
-        Copy-Item -Path $fontFile.FullName -Destination $fontDestinationPath
-
-        # Install the font using AddFontResource
-        $null = AddFontResource -LiteralPath $fontDestinationPath
-        [System.Runtime.InteropServices.Marshal]::ReleaseComObject($null) | Out-Null
-
-        # Check if the font is already registered
-        $fontRegistryPath = "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts"
-        if (Get-ItemProperty -Name $fontName -Path $fontRegistryPath -ErrorAction SilentlyContinue) {
-            Write-Host "Font already registered: $($fontFile.Name)"
-            return
-        }
-
-        # Register the font in the system's font registry
-        Write-Host "Registering font: $($fontFile.Name)"
-        New-ItemProperty -Name $fontName -Path $fontRegistryPath -PropertyType String -Value $fontFile.Name -Force | Out-Null
-
-        Write-Host "Font installed successfully: $($fontFile.Name)"
-    } catch {
-        Write-Host "Error installing font: $($fontFile.Name). $_"
     }
 }
 
-
-Install-Font -fontFile $src
+# Replace "C:\Path\To\Your\FontsDirectory" with the actual path to your fonts directory.
+$srcDirs = Get-ChildItem -Path $src -Include "*.ttf","*.otf" -Recurse
+Install-Font -srcDir $srcDirs
