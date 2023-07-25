3527d56acc63fc94f08f8354445a59189d01c777
Cornelis Terblanche <1997corry@gmail.com>
2023-07-25 03:11:47 +0200

    Basic haulusegenic readme added

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
-# Win_Mass_Font_Installer
+Certainly! Here's the revised GitHub README for the "Win_Bulk_Font_Installer" repository:
+
+# Win Bulk Font Installer - Install Fonts Recursively
+
+![Win Bulk Font Installer](https://github.com/YourGitHubUsername/Win_Bulk_Font_Installer/blob/main/FontInstallerLogo.png)
+
+## Overview
+
+Win Bulk Font Installer is a PowerShell script designed to install fonts contained within multiple directories recursively. The script will scan the specified source directory and its subdirectories for TrueType (.ttf) and OpenType (.otf) font files and then proceed to install each font.
+
+## Features
+
+- Installs fonts contained in multiple directories recursively.
+- Supports TrueType (.ttf) and OpenType (.otf) font formats.
+- Checks if the font is already installed before proceeding.
+- Prompts the user for confirmation before installing each font.
+- Registers fonts in the system's font registry.
+
+## Prerequisites
+
+- PowerShell 3.0 or higher.
+
+## How to Use
+
+1. Clone or download the repository to your local machine.
+
+2. Open a PowerShell terminal.
+
+3. Ensure that PowerShell script execution is allowed on your system. If not, you can enable it by running PowerShell as Administrator and executing the following command:
+
+   ```powershell
+   Set-ExecutionPolicy RemoteSigned
+   ```
+
+   Choose "Y" or "A" when prompted.
+
+4. Change the directory to the location of the script file.
+
+5. Run the script using the following command:
+
+   ```powershell
+   .\Win_Bulk_Font_Installer.ps1 -src "C:\Fonts"
+   ```
+
+   Replace "C:\Fonts" with the path to the source directory containing the font files you want to install.
+
+6. The script will process all the font files found in the source directory and its subdirectories. It will copy each font file to the Fonts directory (C:\Windows\Fonts) and register it in the system's font registry.
+
+7. Before installing each font, the script will prompt you for confirmation. Type "Y" or "N" to proceed or cancel the installation.
+
+## Example
+
+```powershell
+PS C:\Scripts> .\Win_Bulk_Font_Installer.ps1 -src "C:\Fonts"
+Font file not found: C:\Fonts\FontExample.ttf
+Copying font: FontExample.otf
+Are you sure you want to install the font 'FontExample (OpenType)'? (Y/N'): Y
+Registering font: FontExample (OpenType)
+Font installed successfully: FontExample.otf
+Font already installed: AnotherFont.ttf
+Font installation cancelled.
+```
+
+## License
+
+This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).
+
+## Contributions
+
+Contributions to the project are welcome. If you find any issues or have suggestions for improvements, feel free to create a pull request or submit an issue.
+
+## Disclaimer
+
+This script is provided as-is, without any warranty or guarantee of any kind. The author is not responsible for any damage caused by the usage of this script.
+
+## Credits
+
+This script was created by [Cornelis Terblanche](https://github.com/KR34T1V) and inspired by the need for an efficient font installation tool.
+
+---
+
+Thank you for using Win Bulk Font Installer! If you have any questions or feedback, please don't hesitate to reach out. Happy font installing!
--- Win_Bulk_Font_Installer.ps1
+++ Win_Bulk_Font_Installer.ps1
@@ -1,70 +1,82 @@
 param (
     [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-    [System.IO.FileInfo[]]$src
+    [string[]]$srcDir
 )
+
 function Install-Font {
     param (
         [Parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-        [System.IO.FileInfo[]]$srcDir
+        [string]$srcDir
     )
 
-    process {
-        foreach ($file in $srcDir) {
-            try {
-                # Check if the font file exists
-                if (-not $file.Exists) {
-                    Write-Host "Font file not found: $($file.FullName)"
-                    continue
-                }
+    # Function to get the formatted font name with style
+    function Get-FontName {
+        param (
+            [string]$file
+        )
 
-                # Get font name and style
-                $fontName = $file.Name
-                switch ($file.Extension.ToLower()) {
-                    ".ttf" { $fontName += " (TrueType)" }
-                    ".otf" { $fontName += " (OpenType)" }
-                }
+        $extension = [System.IO.Path]::GetExtension($file).ToLower()
+        switch ($extension) {
+            ".ttf" { return "$(Split-Path -Leaf $file) (TrueType)" }
+            ".otf" { return "$(Split-Path -Leaf $file) (OpenType)" }
+            default { return $(Split-Path -Leaf $file) }
+        }
+    }
 
-                # Check if the font is already installed
-                $fontDestinationPath = "$($env:windir)\Fonts\$($file.Name)"
-                if (Test-Path $fontDestinationPath) {
-                    Write-Host "Font already installed: $($file.Name)"
-                    continue
-                }
-
-                # Prompt user for confirmation before proceeding
-                $confirmation = Read-Host "Are you sure you want to install the font '$fontName'? (Y/N)"
-                if ($confirmation -notmatch '^[Yy]$') {
-                    Write-Host "Font installation cancelled."
-                    continue
-                }
-
-                # Copy the font file to the Fonts directory
-                Write-Host "Copying font: $($file.Name)"
-                Copy-Item -Path $file.FullName -Destination $fontDestinationPath
-
-                # Install the font using AddFontResource
-                $result = AddFontResource -LiteralPath $fontDestinationPath
-                [System.Runtime.InteropServices.Marshal]::ReleaseComObject($result) | Out-Null
-
-                # Check if the font is already registered
-                $fontRegistryPath = "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts"
-                if (Get-ItemProperty -Name $fontName -Path $fontRegistryPath -ErrorAction SilentlyContinue) {
-                    Write-Host "Font already registered: $($file.Name)"
-                    continue
-                }
-
-                # Register the font in the system's font registry
-                Write-Host "Registering font: $($file.Name)"
-                New-ItemProperty -Name $fontName -Path $fontRegistryPath -PropertyType String -Value $file.Name -Force | Out-Null
-
-                Write-Host "Font installed successfully: $($file.Name)"
-            } catch {
-                Write-Host "Error installing font: $($file.Name). $_"
+    # Process the fonts in the directory and its subdirectories
+    $fontFiles = Get-ChildItem -Path $srcDir -Include "*.ttf","*.otf" -File -Recurse
+    foreach ($file in $fontFiles) {
+        try {
+            # Check if the font file exists
+            if (-not (Test-Path $file)) {
+                Write-Host "Font file not found: $($file)"
+                continue
             }
+
+            # Get font name and style
+            $fontName = Get-FontName -file $file
+
+            # Check if the font is already installed
+            $fontDestinationPath = "$($env:windir)\Fonts\$($file.Name)"
+            if (Test-Path $fontDestinationPath) {
+                Write-Host "Font already installed: $($file.Name)"
+                continue
+            }
+
+            # Prompt user for confirmation before proceeding
+            $confirmation = Read-Host "Are you sure you want to install the font '$fontName'? (Y/N')"
+            if ($confirmation -notmatch '^[Yy]$') {
+                Write-Host "Font installation cancelled."
+                continue
+            }
+
+            # Copy the font file to the Fonts directory
+            Write-Host "Copying font: $(Split-Path -Leaf $file)"
+            Copy-Item -Path $file -Destination $fontDestinationPath
+
+            # Install the font using AddFontResource
+            $result = Add-FontResource -LiteralPath $fontDestinationPath
+            [System.Runtime.InteropServices.Marshal]::ReleaseComObject($result) | Out-Null
+
+            # Register the font in the system's font registry
+            $registryPath = "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts"
+            if (-not (Test-Path $registryPath)) {
+                Write-Host "Registry path not found: $registryPath"
+                continue
+            }
+
+            if (-not (Get-ItemProperty -Name $fontName -Path $registryPath -ErrorAction SilentlyContinue)) {
+                Write-Host "Registering font: $fontName"
+                New-ItemProperty -Name $fontName -Path $registryPath -PropertyType String -Value $fontName -Force | Out-Null
+                Write-Host "Font installed successfully: $(Split-Path -Leaf $file)"
+            } else {
+                Write-Host "Font already registered: $fontName"
+            }
+        } catch {
+            Write-Host "Error installing font: $(Split-Path -Leaf $file). $_.Exception.Message"
         }
     }
 }
 
-# Replace "C:\Path\To\Your\FontsDirectory" with the actual path to your fonts directory.
-$srcDirs = Get-ChildItem -Path $src -Include "*.ttf","*.otf" -Recurse
-Install-Font -srcDir $srcDirs
+# Call the Install-Font function for each font directory in the source directory
+$srcDir | Install-Font
